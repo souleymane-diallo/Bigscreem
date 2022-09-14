283e38dc0bb8ca5d9806851d26e7b58cdfad4a80
jockthan <jockthanmik@gmail.com>
2022-09-14 09:32:01 +0200

    feat(seeder): create a seeder for the survey

---
+++ database/seeders/SurveyTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Survey;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SurveyTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $survey = [
+            [ 'title'=>"identification eT satisfaction client"]
+        ];
+        Survey::insert($survey);
+    }
+}
