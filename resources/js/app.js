9d5371beeb3b1215b487bff072dca9e5dccd87ff
Souleymane DIALLO <s.diallo.souleymane.d@gmail.com>
2022-09-14 23:54:06 +0200

    feat(breeze): breeze authentification

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,7 @@
 import './bootstrap';
-import '../css/app.css';
-import router from './router';
-import { createApp } from 'vue/dist/vue.esm-bundler';
-import SurveyView from "./views/SurveyView.vue";
-import Navbar from  "./components/Navbar.vue";
-createApp({
-  components: {
-    SurveyView,
-    Navbar
-    
-  }  
-}).use(router).mount("#app")
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
