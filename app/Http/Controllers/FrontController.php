c8f6f8af7cbc1384c7d78ade6659dab97a747189
jockthan <jockthanmik@gmail.com>
2022-09-15 11:06:49 +0200

    feat(Controller): added function answers in the FrontController

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Answer;
 use App\Models\Question;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     public function index(){
-        
+
         $questions = Question::all();
         return view('front.index',['questions'=>$questions]);
 
+    }
+    public function answers(string $single_link) {
+        $questions = Question::all();
+        $answers = Answer::hashPath($single_link)->pluck('response', 'question_id');
+
+        return view('front.answer', ['questions' => $questions, 'answers' => $answers]);
     }
 }
