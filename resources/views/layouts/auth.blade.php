45395e2e1531037d3abc5c6a1e80d14856212dbc
jockthan <jockthanmik@gmail.com>
2022-09-15 10:46:44 +0200

    feat(blade): create front interfaces

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>WeFashion | Login</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+  <link rel="stylesheet" href="{{mix("css/app.css")}}" />
+
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition login-page bg-dark" style="transform: translateY(-80px);">
+@yield("container")
+
+<script src="{{ mix('js/app.js') }}"></script>
+
+</body>
+</html>
