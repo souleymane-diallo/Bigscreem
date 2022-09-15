192c910ca1d159419298ea74c918a21c503c82de
Souleymane DIALLO <s.diallo.souleymane.d@gmail.com>
2022-09-15 16:59:51 +0200

    fix(style): improve page appearance survey

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -3,7 +3,9 @@
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                   <h1 class="text-1xl">Merci de repondre à toutes les questions et de valider le formulaire en bas de page.</h3>
+                    <div class="mb-5">
+                        <h1 class="fon-semibold text-2xl">Merci de repondre à toutes les questions et de valider le formulaire en bas de page.</h3>
+                    </div>
                    
                     <form action="" method="POST">
                         @csrf
@@ -14,16 +16,27 @@
                                 <p class="text-white"> {{ $question->body }} </p>
                                 <div class="border-dashed border-2 border-sky-500 p-2 my-2">
                                     @if ($question->type === 'B')
-                                        <x-text-input 
-                                            id="email" 
-                                            class="block mt-1 w-full" 
-                                            type="email" name="email-{{ $question->id }}" :value="old('email')" required 
-                                        />
+                                        @if ($question->id === 1)
+                                            <x-text-input 
+                                                id="email" 
+                                                class="block mt-1 w-full" 
+                                                type="email" name="email-{{ $question->id }}" :value="old('email')" required
+                                                placeholder="jolene@gmail.com" 
+                                            />
+                                        @else
+                                            <x-text-input 
+                                                id="email" 
+                                                class="block mt-1 w-full" 
+                                                type="text" name="email-{{ $question->id }}" :value="old('email')" required
+                                            />
+                                        @endif
+                                    
                                     @else
                                         <select 
                                             name="" id=""
                                             class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" 
                                         >
+                                            <option>--- Selectionnez votre réponse ----</option>
                                             @foreach (explode(',', $question->possible_answer) as $response)
                                                 <option value="{{ $response }}">{{ $response }}</option>
                                             @endforeach
