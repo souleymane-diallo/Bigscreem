ad1a09e1ff623a9856344ff0284bd68a70e30e00
Souleymane DIALLO <s.diallo.souleymane.d@gmail.com>
2022-09-17 13:42:16 +0200

    feat(admin): dashboard private authentificate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        // register admin in the database for authentificate private
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+        ]);
+
         $this->call([
             SurveyTableSeeder::class,
             QuestionTableSeeder::class
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnswerController;
+use App\Http\Controllers\DashboardController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,17 +18,15 @@ use App\Http\Controllers\AnswerController;
 
 
 Route::get('/', [FrontController::class, 'index']);
-Route::get('/{id}', [FrontController::class, 'answer']);
+Route::get('answer/{id}', [FrontController::class, 'answer']);
 
+Route::get('/administration', [DashboardController::class, 'index'])
+    ->middleware(['auth'])
+    ->name('administration')
+;
 
-/*Route::prefix('administration')->name('admin.')->middleware('auth')->group(function(){
-
-});*/
 Route::resource('answer', AnswerController::class);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
 
