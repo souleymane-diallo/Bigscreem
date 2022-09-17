97fb799855c36395d47abd6423f1baa2dae61bd7
jockthan <jockthanmik@gmail.com>
2022-09-17 16:01:31 +0200

    feat(blade): recuperation de données  d'un sondé

--- resources/views/front/answer.blade.php
+++ resources/views/front/answer.blade.php
@@ -3,30 +3,28 @@
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <h1 class="fon-semibold text-1xl mb-4">Votre trouverez ci-dessous les réponses que avez apportés à notre sondage le 15 septembre 2022 à 13h:32</h3>
-                    <div class="bg-gray-400 p-2 my-2 rounded-md shadow-sm">
-                        <h3 class="font-semibold text-white">Question 1/20</h3>
-                        <p class="text-white"> Votre adresse mail </p>
-                        <div class="border-dashed border-2 border-sky-500 p-2 my-2">
-                            <p>jolene@gmail.com</p>
-                        </div>
-                    </div>
-                    <div class="bg-gray-400 p-2 my-2 rounded-md shadow-sm">
-                        <h3 class="font-semibold text-white">Question 2/20</h3>
-                        <p class="text-white"> Votre age</p>
-                        <div class="border-dashed border-2 border-sky-500 p-2 my-2">
-                            <p>25</p>
-                        </div>
-                    </div>
-                    <div class="bg-gray-400 p-2 my-2 rounded-md shadow-sm">
-                        <h3 class="font-semibold text-white">Question 20/20</h3>
-                        <p class="text-white"> Votre profession </p>
-                        <div class="border-dashed border-2 border-sky-500 p-2 my-2">
-                            <p>Developpeur Front-End</p>
+
+                        @foreach ($questions as $question)
+                        <h1 class="fon-semibold text-1xl mb-4">Votre trouverez ci-dessous les réponses que avez apportés à
+                        notre sondage le {{ $question->created_at}}</h1>
+                            <div class="bg-gray-400 p-2 my-2 rounded-md shadow-sm">
+                                <h3 class="font-semibold text-white">{{ $question->title }}</h3>
+                                <p class="text-white"> {{ $question->body }} </p>
+                                @foreach ($answers as $key => $value)
+                                    @if ($question->id == $key)
+                                        <div class="border-dashed border-2 border-sky-500 p-2 my-2">
+                                            <p>{{ $value }}</p>
+                                        </div>
+                                    @endif
+                                {{-- @empty
+                                    <p>Aucune réponses...</p> --}}
+                                @endforeach
+                            </div>
+                        {{-- @empty
+                            <p>Aucune réponses...</p> --}}
+                        @endforeach
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-</x-app-layout>
+        </x-app-layout>
--- resources/views/front/message.blade.php
+++ resources/views/front/message.blade.php
@@ -3,15 +3,19 @@
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    @if (session('message'))
-                    <div class="alert alert-success alert-dismissible fade show" id="notif-bar" style="margin-top:8px"
-                        role="alert">
-                        {{ session('message') }}
-                        <button type="button" id="close_notif" class="close" data-dismiss="alert"
-                            aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
+                    @if (session('url'))
+                        <div class="alert alert-success alert-dismissible fade show" id="notif-bar" style="margin-top:8px"
+                            role="alert">
+                            <p>"Toute l’équipe de Bigscreen vous remercie pour votre engagement. Grâce à
+                                votre investissement, nous vous préparons une application toujours plus
+                                facile à utiliser, seul ou en famille.
+                                Si vous désirez consulter vos réponse ultérieurement, vous pouvez consultez
+                                cette adresse : <a href=' {{ session('url') }}' />{{ session('url') }}</a></p>
+                            <button type="button" id="close_notif" class="close" data-dismiss="alert"
+                                aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
                     @endif
                 </div>
             </div>
