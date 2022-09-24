23f5aa7d24803146b0eabd2205a8fefac0d0db21
Souleymane DIALLO <s.diallo.souleymane.d@gmail.com>
2022-09-24 11:29:35 +0000

    fix(survey): views

--- resources/views/front/answer.blade.php
+++ resources/views/front/answer.blade.php
@@ -1,28 +1,33 @@
 <x-app-layout>
-    <div class="py-12">
-        <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
+    <div class="">
+        <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <h1 class="fon-semibold text-1xl mb-4">Votre trouverez ci-dessous les réponses que avez apportés à notre sondage le 15 septembre 2022 à 13h:32</h3>
-                    <div class="bg-gray-400 p-2 my-2 rounded-md shadow-sm">
-                        <h3 class="font-semibold text-white">Question 1/20</h3>
-                        <p class="text-white"> Votre adresse mail </p>
-                        <div class="border-dashed border-2 border-sky-500 p-2 my-2">
+                <div class="p-4 bg-gray-600 border-b border-gray-200">
+                    <div class="mb-2">
+                        <img src="{{ asset('image/bigscreen_logo.png') }}" alt="logo BigScreen" width=250 />
+                    </div>
+                    <div class="mb-5">
+                        <h1 class="fon-semibold text-1xl text-white">Votre trouverez ci-dessous les réponses que avez apportés à notre sondage le 15 septembre 2022 à 13h:32</h3>
+                    </div>
+                    <div class="bg-gray-200 p-2 my-2 rounded-md shadow-sm">
+                        <h3 class="font-semibold">Question 1/20</h3>
+                        <p class=""> Votre adresse mail </p>
+                        <div class="border-dashed border-2 border-gray-400 p-2 my-2">
                             <p>jolene@gmail.com</p>
                         </div>
                     </div>
-                    <div class="bg-gray-400 p-2 my-2 rounded-md shadow-sm">
-                        <h3 class="font-semibold text-white">Question 2/20</h3>
-                        <p class="text-white"> Votre age</p>
-                        <div class="border-dashed border-2 border-sky-500 p-2 my-2">
-                            <p>25</p>
+                    <div class="bg-gray-200 p-2 my-2 rounded-md shadow-sm">
+                        <h3 class="font-semibold">Question 1/20</h3>
+                        <p class=""> Votre adresse mail </p>
+                        <div class="border-dashed border-2 border-gray-400 p-2 my-2">
+                            <p>jolene@gmail.com</p>
                         </div>
                     </div>
-                    <div class="bg-gray-400 p-2 my-2 rounded-md shadow-sm">
-                        <h3 class="font-semibold text-white">Question 20/20</h3>
-                        <p class="text-white"> Votre profession </p>
-                        <div class="border-dashed border-2 border-sky-500 p-2 my-2">
-                            <p>Developpeur Front-End</p>
+                    <div class="bg-gray-200 p-2 my-2 rounded-md shadow-sm">
+                        <h3 class="font-semibold">Question 1/20</h3>
+                        <p class=""> Votre adresse mail </p>
+                        <div class="border-dashed border-2 border-gray-400 p-2 my-2">
+                            <p>jolene@gmail.com</p>
                         </div>
                     </div>
                 </div>
