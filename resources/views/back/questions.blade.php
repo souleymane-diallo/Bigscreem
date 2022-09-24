f6829bf50cfbffaf8b1c7a8fe1a84466b7dffe1e
Souleymane DIALLO <s.diallo.souleymane.d@gmail.com>
2022-09-24 11:22:54 +0000

    fix(dashboard): dashboard views for administratio

--- resources/views/back/questions.blade.php
+++ resources/views/back/questions.blade.php
@@ -1,3 +1,35 @@
-<x-app-layout>
-    <h2>Questionnaires</h2>
+<x-app-layout>  
+    <!-- Client Table Questions -->
+    <div class="mt-20 mx-4">
+      <div class="w-full overflow-hidden rounded-lg shadow-xs">
+        <div class="w-full overflow-x-auto">
+          <table class="w-full">
+            <thead>
+              <tr class="text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800">
+                <th class="px-4 py-3">N°</th>
+                <th class="px-4 py-3">Corps de Questions</th>
+                <th class="px-4 py-3">Réponses</th>
+              </tr>
+            </thead>
+            <tbody class="bg-white divide-y dark:divide-gray-700 dark:bg-gray-800">
+              <tr class="bg-gray-50 dark:bg-gray-800 hover:bg-gray-100 dark:hover:bg-gray-900 text-gray-700 dark:text-gray-400">
+                <td class="px-4 py-3">
+                  <div class="flex items-center text-sm">
+                    <div>
+                      <p class="font-semibold">Hans Burger</p>
+                      <p class="text-xs text-gray-600 dark:text-gray-400">10x Developer</p>
+                    </div>
+                  </div>
+                </td>
+                <td class="px-4 py-3 text-sm">$855.85</td>
+                <td class="px-4 py-3 text-xs">
+                  <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"> Approved </span>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <!-- ./Client Table Questions -->
 </x-app-layout>
