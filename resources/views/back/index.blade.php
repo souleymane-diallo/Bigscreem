e74e555f22d3fcea0f1f3b3964909ca1863778b7
jockthan <jockthanmik@gmail.com>
2022-09-27 19:46:51 +0200

    feat(blade): code implementation for displaying graphs

--- resources/views/back/index.blade.php
+++ resources/views/back/index.blade.php
@@ -1,5 +1,5 @@
 <x-app-layout>
-<!-- component -->   
+<!-- component -->
   <!-- ./Statistics Cards -->
   <div class="grid grid-cols-1 lg:grid-cols-2 p-4 gap-4">
 
@@ -14,89 +14,38 @@
             <button class="bg-blue-500 dark:bg-gray-100 text-white active:bg-blue-600 dark:text-gray-800 dark:active:text-gray-700 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">See all</button>
           </div>
         </div>
-        <div class="block w-full overflow-x-auto">
-          <table class="items-center w-full bg-transparent border-collapse">
-            <thead>
-              <tr>
-                <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">Referral</th>
-                <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">Visitors</th>
-                <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px"></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Facebook</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">5,480</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">70%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
-                        <div style="width: 70%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-600"></div>
-                      </div>
+            <div class="row">
+                @forelse($pieDatas as $pieData)
+                    <div class="col-md-6 mt-6">
+                        <p class="question">{{ $pieData['question'] }}</p>
+                        <canvas id="question{{ $pieData['question_id'] }}" height="360"></canvas>
                     </div>
-                  </div>
-                </td>
-              </tr>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Twitter</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">3,380</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">40%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
-                        <div style="width: 40%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Instagram</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">4,105</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">45%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-pink-200">
-                        <div style="width: 45%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-pink-500"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Google</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">4,985</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">60%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-red-200">
-                        <div style="width: 60%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Linkedin</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">2,250</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">30%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
-                        <div style="width: 30%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-700"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+                @empty
+                @endforelse
+
+                <div class="col-md-6 mt-6">
+                    <p class="question"></p>
+                    <canvas id="questionRadar" height="360"></canvas>
+                </div>
+            </div>
+            <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+
+            @forelse($pieDatas as $pieData)
+                <script>
+                    var ctx = document.getElementById("question" + <?= $pieData['question_id'] ?>).getContext('2d');
+                    var myChart = new Chart(ctx, {
+                        type: 'pie',
+                        data: {
+                            labels: @json($pieData['labels']),
+                            datasets: [{
+                                data: @json($pieData['datas']),
+                                backgroundColor: @json($pieData['colors'])
+                            }]
+                        }
+                    });
+                </script>
+            @empty
+            @endforelse
       </div>
     </div>
 
@@ -110,92 +59,41 @@
             <button class="bg-blue-500 dark:bg-gray-100 text-white active:bg-blue-600 dark:text-gray-800 dark:active:text-gray-700 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">See all</button>
           </div>
         </div>
-        <div class="block w-full overflow-x-auto">
-          <table class="items-center w-full bg-transparent border-collapse">
-            <thead>
-              <tr>
-                <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">Referral</th>
-                <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">Visitors</th>
-                <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px"></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Facebook</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">5,480</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">70%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
-                        <div style="width: 70%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-600"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Twitter</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">3,380</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">40%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
-                        <div style="width: 40%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Instagram</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">4,105</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">45%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-pink-200">
-                        <div style="width: 45%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-pink-500"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Google</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">4,985</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">60%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-red-200">
-                        <div style="width: 60%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Linkedin</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">2,250</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">30%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
-                        <div style="width: 30%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-700"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+        <script>
+                    var ctx = document.getElementById("questionRadar").getContext('2d');
+                    var myChart = new Chart(ctx, {
+                        type: 'radar',
+                        data: {
+                            /**
+                             * labels = ID de la Question
+                             * data = Moyenne obtenue par la question
+                             */
+                            labels: @json($radarDatas['labels']),
+                            datasets: [{
+                                label: "Moyenne",
+                                data: @json($radarDatas['datas']),
+                                backgroundColor: "rgba(179,181,198,0.2)",
+                                borderColor: "rgba(179,181,198,1)",
+                                pointBackgroundColor: "rgba(179,181,198,1)",
+                                pointBorderColor: "#fff",
+                                pointHoverBackgroundColor: "#fff",
+                                pointHoverBorderColor: "rgba(179,181,198,1)",
+                            }]
+                        },
+                        options: {
+                            scale: {
+                                ticks: {
+                                    beginAtZero: true,
+                                    max: 5,
+                                    stepSize: 1
+                                }
+                            }
+                        }
+                    });
+                </script>
       </div>
     </div>
-    <!-- ./Social Traffic -->
+
   </div>
 
 </x-app-layout>
