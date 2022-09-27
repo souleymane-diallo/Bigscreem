3e32248c5dd585ec5bb10bf03f1b3c427f50bb92
Souleymane DIALLO <s.diallo.souleymane.d@gmail.com>
2022-09-27 18:00:09 +0000

    fix(chart): add css grid

--- resources/views/back/index.blade.php
+++ resources/views/back/index.blade.php
@@ -1,6 +1,5 @@
 <x-app-layout>
-<!-- component -->   
-  <!-- ./Statistics Cards -->
+
   <div class="grid grid-cols-1 lg:grid-cols-2 p-4 gap-4">
 
     <!-- Social Traffic -->
@@ -8,94 +7,11 @@
       <div class="rounded-t mb-0 px-0 border-0">
         <div class="flex flex-wrap items-center px-4 py-2">
           <div class="relative w-full max-w-full flex-grow flex-1">
-            <h3 class="font-semibold text-base text-gray-900 dark:text-gray-50">Social Traffic</h3>
-          </div>
-          <div class="relative w-full max-w-full flex-grow flex-1 text-right">
-            <button class="bg-blue-500 dark:bg-gray-100 text-white active:bg-blue-600 dark:text-gray-800 dark:active:text-gray-700 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">See all</button>
+            <h3 class="font-semibold text-base text-gray-900 dark:text-gray-50">PIE CHART</h3>
           </div>
         </div>
         <div class="block w-full overflow-x-auto">
-          <table class="items-center w-full bg-transparent border-collapse">
-            <thead>
-              <tr>
-                <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">Referral</th>
-                <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">Visitors</th>
-                <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px"></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Facebook</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">5,480</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">70%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
-                        <div style="width: 70%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-600"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Twitter</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">3,380</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">40%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
-                        <div style="width: 40%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Instagram</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">4,105</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">45%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-pink-200">
-                        <div style="width: 45%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-pink-500"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Google</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">4,985</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">60%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-red-200">
-                        <div style="width: 60%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Linkedin</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">2,250</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">30%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
-                        <div style="width: 30%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-700"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <canvas id="myChart"></canvas>
         </div>
       </div>
     </div>
@@ -104,98 +20,49 @@
       <div class="rounded-t mb-0 px-0 border-0">
         <div class="flex flex-wrap items-center px-4 py-2">
           <div class="relative w-full max-w-full flex-grow flex-1">
-            <h3 class="font-semibold text-base text-gray-900 dark:text-gray-50">Social Traffic</h3>
-          </div>
-          <div class="relative w-full max-w-full flex-grow flex-1 text-right">
-            <button class="bg-blue-500 dark:bg-gray-100 text-white active:bg-blue-600 dark:text-gray-800 dark:active:text-gray-700 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">See all</button>
+            <h3 class="font-semibold text-base text-gray-900 dark:text-gray-50">Radar Char</h3>
           </div>
         </div>
         <div class="block w-full overflow-x-auto">
-          <table class="items-center w-full bg-transparent border-collapse">
-            <thead>
-              <tr>
-                <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">Referral</th>
-                <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">Visitors</th>
-                <th class="px-4 bg-gray-100 dark:bg-gray-600 text-gray-500 dark:text-gray-100 align-middle border border-solid border-gray-200 dark:border-gray-500 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left min-w-140-px"></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Facebook</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">5,480</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">70%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
-                        <div style="width: 70%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-600"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Twitter</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">3,380</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">40%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
-                        <div style="width: 40%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-500"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Instagram</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">4,105</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">45%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-pink-200">
-                        <div style="width: 45%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-pink-500"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Google</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">4,985</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">60%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-red-200">
-                        <div style="width: 60%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-red-500"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-              <tr class="text-gray-700 dark:text-gray-100">
-                <th class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left">Linkedin</th>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">2,250</td>
-                <td class="border-t-0 px-4 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
-                  <div class="flex items-center">
-                    <span class="mr-2">30%</span>
-                    <div class="relative w-full">
-                      <div class="overflow-hidden h-2 text-xs flex rounded bg-blue-200">
-                        <div style="width: 30%" class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-blue-700"></div>
-                      </div>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <div>
+            <canvas id="myChart2"></canvas>
+          </div>
         </div>
       </div>
     </div>
     <!-- ./Social Traffic -->
+    <script type="text/javascript">
+      const labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+      ];
+
+      const data = {
+        labels: labels,
+        datasets: [{
+          label: 'My First dataset',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+      };
+
+      const config = {
+        type: 'line',
+        data: data,
+        options: {}
+      };
+
+      const myChart = new Chart(
+        document.getElementById('myChart'),
+        config
+      );
+
+    </script>
   </div>
 
 </x-app-layout>
