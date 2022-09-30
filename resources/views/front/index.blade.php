a6d34fcdbc364dd59eeed58ab9e1c1bd766942b5
jockthan <jockthanmik@gmail.com>
2022-09-30 11:38:19 +0200

    feat(blade): We have added a tag to display an error message when using an email address that already exists

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -7,62 +7,60 @@
                         <img src="{{ asset('image/bigscreen_logo.png') }}" alt="logo BigScreen" width=250 />
                     </div>
                     <div class="mb-5">
-                        <h1 class="fon-semibold text-1xl text-white">Merci de repondre à toutes les questions et de valider le formulaire en bas de page.</h3>
+                        <h1 class="fon-semibold text-1xl text-white">Merci de repondre à toutes les questions et de
+                            valider le formulaire en bas de page.</h3>
                     </div>
                     <!-- Erreurs de validation -->
                     <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
                     <form action="{{ route('answer.store') }}" method="POST">
                         @csrf
-                            @forelse ($questions as $question)
+                        @forelse ($questions as $question)
                             <div class="bg-gray-200 p-2 my-2 rounded-md shadow-sm">
                                 <h3 class="font-semibold">{{ $question->title }}</h3>
                                 <p> {{ $question->body }} </p>
                                 <div class="border-dashed border-2 border-gray-800 p-2 my-2">
                                     @if ($question->type === 'B')
-                                         @if( $question->check_email )
-                                            <x-text-input
-                                                name="email[{{ $question->id }}]"
-                                                id="email"
-                                                :value="old('email[{{ $question->id }}]')"
-                                                class="block mt-1 w-full"
-                                                type="email"
-                                                required
-                                            />
+                                        @if ($question->check_email)
+                                            <span style="color:red">{{ $mail }}</span>
+                                            <x-text-input name="email[{{ $question->id }}]" id="email"
+                                                :value="old('email[{{ $question->id }}]')" class="block mt-1 w-full" type="email" />
+                                            @error('email[{{ $question->id }}]')
+                                                <span class="text-danger"></span>
+                                            @enderror
+                                            <span class="text-danger">test1</span>
                                         @else
-                                            <x-text-input
-                                                name="answer{{ $question->type }}[{{ $question->id }}]"
-                                                id="answer{{ $question->id }}"
-                                                class="block mt-1 w-full"
-                                                type="text"
-                                                :value="old('answer{{ $question->type }}[{{ $question->id }}]')"
-                                                required
-                                             />
+                                            <x-text-input name="answer{{ $question->type }}[{{ $question->id }}]"
+                                                id="answer{{ $question->id }}" class="block mt-1 w-full" type="text"
+                                                :value="old('answer{{ $question->type }}[{{ $question->id }}]')" r />
+                                            @error('answer{{ $question->type }}[{{ $question->id }}]')
+                                                <span class="text-danger">{{ $message }}</span>
+                                            @enderror
+                                            <span class="text-danger">test2</span>
                                         @endif
-
                                     @else
-                                        <select
-                                        name="answer{{ $question->type }}[{{ $question->id }}]"
-                                        id="answer{{$question->id}}"
-                                            class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                        >
+                                        <select name="answer{{ $question->type }}[{{ $question->id }}]"
+                                            id="answer{{ $question->id }}"
+                                            class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
                                             <option value="">--- Selectionnez votre réponse ----</option>
                                             @foreach (explode(',', $question->possible_answer) as $answer)
-                                                <option
-                                                    @selected(old('possible_answer'))
-                                                    value="{{ $answer }}"
-                                                >
+                                                <option @selected(old('possible_answer')) value="{{ $answer }}">
                                                     {{ $answer }}
                                                 </option>
                                             @endforeach
+
                                         </select>
+                                        @error('answer{{ $question->type }}[{{ $question->id }}]')
+                                            <span class="text-danger">{{ $message }}</span>
+                                        @enderror
+                                        <span class="text-danger">test3</span>
                                     @endif
                                 </div>
                             </div>
 
-                            @empty
-                                <p>Aucune questions...</p>
-                            @endforelse
+                        @empty
+                            <p>Aucune questions...</p>
+                        @endforelse
 
                         <div class="flex items-center justify-end mt-2">
                             <x-primary-button class="ml-3">
@@ -76,4 +74,3 @@
         </div>
     </div>
 </x-app-layout>
-
